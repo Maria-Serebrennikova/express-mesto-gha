44d0dc602bc124ff95d625f454b0d4ef964bb6b3
Maria Serebrennikova <maria.silver2015@yandex.ru>
2022-06-20 14:53:20 +0300

    fix: correct cards route

--- routes/cards.js
+++ routes/cards.js
@@ -8,7 +8,7 @@ const {
 } = require('../controllers/cards');
 
 router.get('/cards', getCards);
-router.get('/cards/:cardId', deleteCard);
+router.delete('/cards/:cardId', deleteCard);
 router.post('/cards', createCard);
 router.put('/cards/:cardId/likes', likeCard);
 router.delete('/cards/:cardId/likes', dislikeCard);
